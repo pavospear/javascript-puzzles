70d25065d318dbffb9c89e7eedd23b46654ca66f
Vadivel Murugesan <PAVOSPEAR@gmail.com>
2017-02-28 09:35:06 -0500

    Update braces-validator.js

--- braces-validator.js
+++ braces-validator.js
@@ -1,5 +1,35 @@
 /**
- * Js file to validate the braces in string are balanced or not.
+ ***************
+Braces in a string are considered to be balanced if the following criteria are met:
+For every opening brace (i.e., (, {, or [), there is a matching closing brace (i.e., ), }, or ]) of the same type (i.e., ( matches ), { matches }, and [ matches ]). An opening brace must appear before (to the left of) its matching closing brace. For example, ]{}[ is not balanced.
+No unmatched braces lie between some pair of matched braces. For example, ({[]}) is balanced, but {[}]and [{)] are not balanced.
+ 
+Complete the braces function in the editor below. It has one parameter: an array of n strings, values. For each string in values, it must determine if all the braces in the string are balanced. The function must return an array of strings where the string at each index i (where 0 ≤ i < n) denotes whether or not all the braces in string valuesi were balanced. If yes, then index i in the return array must contain the string YES; otherwise, index i in the return array must contain the string NO.
+ 
+Input Format
+Locked stub code in the editor reads the following input from stdin and passes it to the function:
+The first line contains an integer, n, denoting the number of elements in values.
+Each line i of the n subsequent lines (where 0 ≤ i < n) contains a string describing valuesi.
+ 
+Constraints
+1 ≤ n ≤ 15
+1 ≤ length of valuesi ≤ 100
+It is guaranteed that each valuesi consists of (, ), {, }, [, and ] only.
+ 
+Output Format
+The function must return an array of strings where each index i contains either YES or NO denoting whether or not valuesi was balanced. This is printed to stdout by locked stub code in the editor.
+ 
+Sample Input 0
+{}[]()
+{[}]}
+ 
+Sample Output 0
+YES
+NO
+ 
+Explanation 0
+values0: {}[]() meets the criteria for a balanced string, so index 0 in our return array should contain the string YES.
+values1: {[}] contains unmatched braces between a matched pair in the substrings [}, {[}, and [}], so index 1 in our return array should contain the string NO.
  * 
  * @author vadivel murugesan
  */
